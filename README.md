09308c2a49479c8f603881d36c6584da96d6a1db
carseason <757841203@qq.com>
2021-02-04 02:35:04 +0800

    new docs

---
+++ README.md
@@ -0,0 +1,12 @@
+# 
+    https://vuepress.vuejs.org/zh/
+    
+# 运行
+    npm run docs:dev
+    http://localhost:8080/
+
+# 开始写作
+vuepress dev .
+
+# 构建静态文件
+vuepress build .
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,89 @@
+module.exports = {
+    host: "0.0.0.0",
+    port: 8080,
+    dest: "build",
+    // 部署站点的基础路径
+    base: "/",
+    // 站点标题
+    title: 'DDNSTO',
+    // 网站的描述
+    description: 'DDNSTO 内网穿透,如此简单',
+    head: [
+        [
+            'link', { rel: 'icon', href: '/logo.png' }
+        ]
+    ],
+    // 多语言
+    locales: {
+        '/': {
+            lang: '简体中文',
+            title: 'DDNSTO',
+            description: 'DDNSTO内网穿透',
+        },
+        '/en/': {
+            lang: 'English',
+            title: 'DDNSTO',
+            description: 'DDNSTO'
+        },
+    },
+    themeConfig: {
+        // 站点logo
+        logo: 'https://www.ddnsto.com/linkease/src/image/logo.png',
+        repo: 'https://github.com/linkease/linkease-doc-web',
+        smoothScroll: true,
+        // 页面配置
+        locales: {
+            // 中文
+            '/': {
+                label: '简体中文',
+                selectText: '选择语言',
+                ariaLabel: '选择语言',
+                editLinkText: '在 GitHub 上编辑此页',
+                lastUpdated: '上次更新',
+                nav: [
+                    { text: '首页', link: '/' },
+                    { text: '指南', link: '/zh/guide/' },
+                    { text: '文档', link: '/zh/docs/' },
+                    { text: '下载', link: '/zh/download/' },
+                ],
+                sidebar: {
+                    '/zh/guide/': [
+                        {
+                            title: "测试",
+                            // 显示所有页面的标题链接
+                            displayAllHeaders: true,
+                            // 展开状态
+                            collapsable: false,
+                            sidebarDepth: 2,
+                            children: [
+                                ["", "介绍"],
+                                "show",
+                                "view",
+                            ]
+                        },
+                    ]
+                }
+            },
+            // 英文
+            '/en/': {
+                label: 'English',
+                selectText: 'Languages',
+                ariaLabel: 'Select language',
+                editLinkText: 'Edit this page on GitHub',
+                lastUpdated: 'Last Updated',
+                nav: [
+                    { text: 'home', link: '/' },
+                    { text: 'guide', link: '/zh/guide/' },
+                    { text: 'docs', link: '/zh/docs/' },
+                    { text: 'download', link: '/zh/download/' },
+                ],
+            },
+
+        },
+        // 最后更新时间
+        lastUpdated: 'Last Updated', // string | boolean
+        // 搜索
+        // search: false,
+        // searchMaxSuggestions: 10
+    }
+}
